870ad2cc613a3632f0965faedac4d208b4267635
James1218 <jamesyang1218@gmail.com>
2017-11-18 23:19:08 -0700

    daily update

--- se8/ocp/CH9_JDBC.java
+++ se8/ocp/CH9_JDBC.java
@@ -7,20 +7,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.print.attribute.standard.MediaSize.JIS;
+
 public class CH9_JDBC {
 
 	public static void main(String[] args) {
 
 		String url = "jdbc:derby:zoo";
 		try (Connection connection = DriverManager.getConnection(url);
-				Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)){
-			
+				Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);){
+
 			ResultSet resultSet = statement.executeQuery("select name from animal");
 			while (resultSet.next()){
 				String s = resultSet.getString(1);
 				System.out.println(s);
 			}
-			
 			boolean isResultSet = statement.execute("select name from animal");
 			System.out.println(isResultSet);
 			if (isResultSet){
@@ -33,7 +34,7 @@ public class CH9_JDBC {
 				int rows = statement.getUpdateCount();
 				System.out.println(rows);
 			}
-			
+
 			PreparedStatement preparedStatement = connection.prepareStatement("select * from animal where name = ?");
 			preparedStatement.setString(1, "Elsa");
 			isResultSet = preparedStatement.execute();
@@ -47,12 +48,72 @@ public class CH9_JDBC {
 					System.out.println(id + " "+name+" "+timeDate);
 				}
 			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
 
+			ResultSet rs =  statement.executeQuery("select id, name from species");
+			while(rs.next()) {
+				Object idField = rs.getObject("id");
+				Object nameField = rs.getObject("name");
+				if (idField instanceof Integer) {
+					int id = (Integer) idField;
+					System.out.println(id);
+				}
+				if (nameField instanceof String) {
+					String name = (String) nameField;
+					System.out.println(name);
+				}
+			}
+
+			preparedStatement = connection.prepareStatement("select * from animal order by id ",ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			isResultSet = preparedStatement.execute(); 
+			if(isResultSet){
+				resultSet = preparedStatement.getResultSet();
+				boolean isFirst = resultSet.first();
+				System.out.println("First() : " + isFirst);
+				boolean isLast = resultSet.last();
+				System.out.println("Last() : "+isLast);
+				resultSet.beforeFirst();
+				resultSet.next();
+				System.out.println(resultSet.getInt("id"));
+				resultSet.afterLast();
+				resultSet.previous();
+				System.out.println(resultSet.getInt("id"));
+
+				System.out.println(resultSet.absolute(2));        // true
+				System.out.println(resultSet.getString("id"));    // 2
+				System.out.println(resultSet.absolute(0));        // false, move to before first
+				System.out.println(resultSet.absolute(5));        // true
+				System.out.println(resultSet.getString("id"));    // 5
+				System.out.println(resultSet.absolute(-2));       // true
+				System.out.println(resultSet.getString("id"));    // 4
+
+
+				resultSet.absolute(0);
+				System.out.println(resultSet.next());          // true
+				System.out.println(resultSet.getString("id")); // 1
+				System.out.println(resultSet.relative(2));     // true
+				System.out.println(resultSet.getString("id")); // 3
+				System.out.println(resultSet.relative(-1));    // true
+				System.out.println(resultSet.getString("id")); // 2
+				System.out.println(resultSet.relative(4));     // false
+				
+				int result = statement.executeUpdate("update animal set name = name");
+				System.out.println(result);
+				
+				resultSet = statement.executeQuery("select * from animal");
+				resultSet.next();
+				resultSet.next();
+				System.out.println("default forward only : "+resultSet.absolute(1));
+				
+			}
+
+
+
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getSQLState());
+			System.out.println(e.getErrorCode());
+		}
 		try {
 			//Class.forName() loads a class.
 			Class.forName("org.postgresql.Driver");
@@ -60,8 +121,6 @@ public class CH9_JDBC {
 					"jdbc:postgresql://localhost:5432/ocp-book",
 					"username",
 					"password");
-			
-			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
